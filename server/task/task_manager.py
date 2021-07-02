b617ecaa382bf8727eefe9cdf156755e557964da
Tamir Tapiro <tapirotamirsh@gmail.com>
2021-07-02 13:06:59 +0300

    check that task has 'name' in request body

--- server/task/task_manager.py
+++ server/task/task_manager.py
@@ -45,6 +45,8 @@ def get_all_completed_tasks():
 
 @task_manager_blueprint.route('/add', methods=['POST'])
 def add_task():
+    if 'name' not in request.get_json().keys():
+        return response('request must have body with a new name', 403)
     t = request.get_json()
     print(t)
     if tasks_collection.find_one({'$and': [{'owner': session['user']}, {'name': t['name']}]}) is not None:
@@ -55,32 +57,6 @@ def add_task():
     print(task)
     tasks_collection.insert(task.__dict__)
     return response(f'added new task - [{task.name}]')
-
-# @task_manager_blueprint.route('/update/<id_or_name>', methods=['PUT'])
-# def update_task(id_or_name):
-#     if 'name' not in request.get_json().keys():
-#         return response('request must have body with a new name', 403)
-#     new_name = request.get_json()['name']
-#     update_field = {'$set': {'name': new_name, 'last_updated': datetime.now()}}
-#     if ObjectId.is_valid(id_or_name):
-#         obj_id = ObjectId(id_or_name)
-
-#         t = tasks_collection.find_one_and_update(
-#             {'_id': obj_id},
-#             update_field,
-#             return_document = ReturnDocument.BEFORE
-#         )
-#     else:
-#         t = tasks_collection.find_one_and_update(
-#             {'name': id_or_name},
-#             update_field,
-#             return_document = ReturnDocument.BEFORE
-#         )
-#     if t is None:
-#         return response(f'{id_or_name} is not valid id, nor a task name.', 400)
-#     else:
-#         task = Task(**t)
-#         return response(f'{id_or_name} has been updated from [{task.name}] to [{new_name}].')
 
 @task_manager_blueprint.route('/update/<id_or_name>', methods=['PUT'])
 def update_task(id_or_name):
@@ -98,7 +74,7 @@ def update_task(id_or_name):
     else:
         task = Task(**t)
         if task.completed == True:
-            return response(f'task [{id_or_name}] is completed. you cannot update a completed task.')
+            return response(f'task [{id_or_name}] is completed. you cannot update a completed task.', 402)
         else:
             t = tasks_collection.find_one_and_update(
             {'name': id_or_name},
