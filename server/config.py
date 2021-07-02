39be2e402a473436e2925605699539d844f8d837
Tamir Tapiro <tapirotamirsh@gmail.com>
2021-07-02 13:04:58 +0300

    added a config file

---
+++ server/config.py
@@ -0,0 +1,8 @@
+# config.py
+DEBUG = True
+SECRET_KEY = 'TheMostSecretKeyInTheWrold'
+SESSION_DURATION = 120
+MONGO_URI = "***********************************************"
+MY_DB = "todo_database"
+TASKS_COLLECTION = "tasks"
+USERS_COLLECTION = "users"
